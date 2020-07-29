4c66851cc30dab687723d078a304636139b78a55
unknown <fmansouri@ce.sharif.edu>
2020-07-30 01:49:33 +0430

    ThreeTier : features : sort cards

--- ThreeTier/src/selab/threetier/presentation/TasksListPresentation.java
+++ ThreeTier/src/selab/threetier/presentation/TasksListPresentation.java
@@ -3,13 +3,28 @@ package selab.threetier.presentation;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import selab.threetier.logic.Task;
+
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
+class CustomComparator implements Comparator<Task> {
+    @Override
+    public int compare(Task o1, Task o2) {
+        return o1.getStart().compareTo(o2.getStart());
+    }
+}
 
 public class TasksListPresentation extends JSONPresentation {
     @Override
     public JSONObject getData(String method, InputStream body) {
         JSONObject result = new JSONObject();
-        result.put("tasks", new JSONArray(Task.getAll()));
+        // Changed Here
+        ArrayList<Task> allTasks = Task.getAll();
+        Collections.sort(allTasks, new CustomComparator());
+
+        result.put("tasks", new JSONArray(allTasks));
         return result;
     }
 }
